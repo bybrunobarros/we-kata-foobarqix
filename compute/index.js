9ca0ce18f0336189ad43a7fedb676291ed2b152b
bruno <bybrunobarros@gmail.com>
2020-10-28 15:39:01 +0100

    should return "Bar" when input is 10: ok

--- compute/index.js
+++ compute/index.js
@@ -1,5 +1,15 @@
 export function compute(input) {
-  const output = input % 3 === 0 ? "Foo" : "";
+  const TOKENS = [
+    [3, "Foo"],
+    [5, "Bar"],
+  ];
+
+  const output = TOKENS.reduce((acc, [divisor, word]) => {
+    if (input % divisor === 0) {
+      acc = `${acc}${word}`;
+    }
+    return acc;
+  }, "");
 
   return output || input.toString();
 }
