074a0dc8948958f42290c7d3f71d326f6840d333
bruno <bybrunobarros@gmail.com>
2020-10-28 16:00:15 +0100

    should return "1*1" when input is 101: ok

--- compute/index.js
+++ compute/index.js
@@ -1,9 +1,15 @@
 export function compute(input) {
   const TOKENS = [
+    [0, "*"],
     [3, "Foo"],
     [5, "Bar"],
     [7, "Qix"],
   ];
+  const words = [...TOKENS]
+    .splice(1)
+    .map(([, word]) => word)
+    .join("|");
+  const wordsRegExp = new RegExp(words, "g");
 
   const prefix = TOKENS.reduce((acc, [divisor, word]) => {
     if (input % divisor === 0) {
@@ -17,9 +23,13 @@ export function compute(input) {
     .split("")
     .map((char) => {
       const token = TOKENS.find(([divisor]) => divisor.toString() === char);
-      return token ? token[1] : "";
+      return token ? token[1] : char;
     })
     .join("");
+
+  if (prefix || wordsRegExp.test(suffix)) {
+    suffix = suffix.replace(/\d/g, "");
+  }
 
   return `${prefix}${suffix}` || input.toString();
 }
