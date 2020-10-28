dbbfee89260568e2d9f6b89946887b46a7674792
bruno <bybrunobarros@gmail.com>
2020-10-28 15:26:57 +0100

    should return "Foo" when input is 6: ko

--- compute/index.test.js
+++ compute/index.test.js
@@ -5,3 +5,7 @@ test(`should return "1" when input is 1`, (t) => {
   const computed = compute(1);
   t.is(computed, "1");
 });
+test(`should return "Foo" when input is 6`, (t) => {
+  const computed = compute(6);
+  t.is(computed, "Foo");
+});
