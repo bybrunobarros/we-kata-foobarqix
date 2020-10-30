f99fc61255f8c745b741957a6d3d1ae21528b022
bruno <bybrunobarros@gmail.com>
2020-10-30 12:38:28 +0100

    rework test messages to show pure behaviour

--- compute/index.test.js
+++ compute/index.test.js
@@ -5,28 +5,17 @@ function macro(t, input, expected) {
   const computed = compute(input);
   t.is(computed, expected);
 }
-macro.title = (providedTitle, input, expected) =>
-  `should return "${expected}" when input is ${input}`;
 
-[
-  { input: 1, expected: "1" },
-  { input: 2, expected: "2" },
-  { input: 3, expected: "FooFoo" },
-  { input: 4, expected: "4" },
-  { input: 5, expected: "BarBar" },
-  { input: 6, expected: "Foo" },
-  { input: 7, expected: "QixQix" },
-  { input: 8, expected: "8" },
-  { input: 9, expected: "Foo" },
-  { input: 10, expected: "Bar*" },
-  { input: 13, expected: "Foo" },
-  { input: 15, expected: "FooBarBar" },
-  { input: 21, expected: "FooQix" },
-  { input: 33, expected: "FooFooFoo" },
-  { input: 51, expected: "FooBar" },
-  { input: 53, expected: "BarFoo" },
-  { input: 101, expected: "1*1" },
-  { input: 105, expected: "FooBarQix*Bar" },
-  { input: 303, expected: "FooFoo*Foo" },
-  { input: 10101, expected: "FooQix**" },
-].forEach(({ input, expected }) => test(macro, input, expected));
+test("should write input value when no rule is applicable", macro, 1, "1");
+test(
+  "should write a specific word when the input value is divisible by a specific number",
+  macro,
+  9,
+  "Foo",
+);
+test(
+  "should write a specific word when a specific number is found in the input value",
+  macro,
+  13,
+  "Foo",
+);
