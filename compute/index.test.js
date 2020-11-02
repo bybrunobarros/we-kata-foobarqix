b98b605f9f2af6e935853d8a888030e01a7a4fed
bruno <bybrunobarros@gmail.com>
2020-11-02 12:04:12 +0100

    rephrase test cases for better readability

--- compute/index.test.js
+++ compute/index.test.js
@@ -7,17 +7,17 @@ function macro(t, input, expected) {
 }
 
 test(
-  "should return value when value is not divisible by 3, 5, or 7 and 3, 5, 7, or * are not found",
+  "should return value when value is not divisible by 3, 5, or 7, and does not contain 3, 5, 7, or *",
   macro,
   1,
   "1",
 );
 test("should return Foo when value is only divisible by 3", macro, 9, "Foo");
 test("should return Qix when value is only divisible by 7", macro, 49, "Qix");
-test("should return Foo when 3 only is found", macro, 13, "Foo");
-test("should return Qix when 7 only is found", macro, 17, "Qix");
+test("should return Foo when only contains one 3", macro, 13, "Foo");
+test("should return Qix when only contains one 7", macro, 17, "Qix");
 test(
-  "should return BarBar when value is only divisible by 5 and 5 is found",
+  "should return BarBar when value is divisible by 5 and contains one 5",
   macro,
   5,
   "BarBar",
@@ -28,15 +28,15 @@ test(
   15,
   "FooBarBar",
 );
-test("should return 1*1 when 0 only is found", (t) => {
+test("should return 1*1 when contains one 0", (t) => {
   const computed = compute(101);
   t.is(computed, "1*1");
 });
-test("should return Bar* when value is only divisible by 5 and 0 is found", (t) => {
+test("should return Bar* when value is only divisible by 5, and contains one 0", (t) => {
   const computed = compute(10);
   t.is(computed, "Bar*");
 });
-test("should return FooQix** when value is divisible by 3 and 7 and 0 is found", (t) => {
+test("should return FooQix** when value is divisible by 3 then 7, and contains two 0", (t) => {
   const computed = compute(10101);
   t.is(computed, "FooQix**");
 });
